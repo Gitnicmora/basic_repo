4538f7bfcc6982185a746da649921336460f2cc8
Nicholas <nicholas.mora78@yahoo.com>
2021-02-03 09:55:12 -0800

    These are my example files for my remote repo

---
+++ wordplay.js
@@ -0,0 +1,13 @@
+var friends = 3;
+var you = 1;
+
+function letsEat(){
+    for(var spaghetti = 3; spaghetti > 0; spaghetti--){
+        if(spaghetti == 0){
+            break;
+        }
+    }
+
+}
+console.log('im upsetti they forgetti its my spaghetti')
+letsEat();
